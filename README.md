e0e7d5454507f71ccb369ef38efeb40e33005831
Rafael Azevedo <rafael.nqda@gmail.com>
2022-03-18 18:20:26 +0000

    Add initial implementation

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # commitlint-config-untile
+
+Untile-flavored commitlint config.
+
+## Installation
+
+```sh
+$ yarn add @commitlint/cli @untile/commitlint-config-untile --dev
+```
+
+## Usage
+
+Create an `.commitlintrc.js` file with the following:
+
+```js
+module.exports = {
+  extends: ['@untile/commitlint-config-untile']
+};
+```
+
+## Rules
+- Commit header should start to: **Add|Bump|Fix|Improve|Release|Remove|Update**.
+- Commit header must not be longer than **52** characters.
+- Commit header must have only **alphanumeric** characters.
+- Commit body should be in sentence-case.
+- Commit body should have a full stop.
+
+**NOTE**
+Follow the [commitlint.js.org](https://commitlint.js.org/#/guides-local-setup?id=install-husky) guide 
+to see how to finish the configuration, using for example husky.
+
+## Releases
+
+Be sure to have configured `GITHUB_TOKEN` in your globals.
+
+```bash
+npm version [<new version> | major | minor | patch] -m "Release %s"
+git push origin master && git push --tags
+```
---
+++ test/index.test.js
@@ -0,0 +1,92 @@
+
+/**
+ * Module dependencies.
+ */
+
+const commitlint = require('@commitlint/lint');
+const config = require('../src');
+const load = require('@commitlint/load');
+
+/**
+ * Lint.
+ */
+
+const lint = async message => {
+  const preparedConfig = await load.default(config);
+
+  return commitlint.default(message, preparedConfig.rules, {
+    parserOpts: preparedConfig.parserPreset.parserOpts,
+    ...preparedConfig
+  });
+};
+
+/**
+  * `commitlint-config-untile` tests.
+  */
+
+describe('commitlint-config-untile', () => {
+  describe('correct', () => {
+    it('should return true if the commit complies with the rules', () => {
+      return lint('Add foobar').then(result => {
+        expect(result.valid).toBe(true);
+      });
+    });
+
+    it('should return true if the commit starts with `wip`', () => {
+      return lint('wip').then(result => {
+        expect(result.valid).toBe(true);
+      });
+    });
+  });
+
+  describe('incorrect', () => {
+    it('should return an error if the commit not start with first letter in uppercase', () => {
+      return lint('add foobar').then(result => {
+        expect(result.valid).toBe(false);
+        expect(result.errors[0].name).toEqual('function-rules/type-enum');
+      });
+    });
+
+    it('should return an error if the commit not start with (Add|Bump|Fix|Improve|Release|Remove|Update)', () => {
+      return lint('Foo bar').then(result => {
+        expect(result.valid).toBe(false);
+        expect(result.errors[0].name).toEqual('function-rules/type-enum');
+      });
+    });
+
+    it('should return an error if the commit exceed the 52 chars limit', () => {
+      return lint('Add lorem ipsum is simply dummy text of the printing a').then(result => {
+        expect(result.valid).toBe(false);
+        expect(result.errors[0].name).toEqual('header-max-length');
+      });
+    });
+
+    it('should return an error if the commit has only one word', () => {
+      return lint('Add').then(result => {
+        expect(result.valid).toBe(false);
+        expect(result.errors[0].name).toEqual('function-rules/type-enum');
+      });
+    });
+
+    it('should return an error if the commit has an empty space in the end', () => {
+      return lint('Add foo ').then(result => {
+        expect(result.valid).toBe(false);
+        expect(result.errors[0].name).toEqual('function-rules/type-enum');
+      });
+    });
+
+    it('should return an error if the commit has extra spaces between words', () => {
+      return lint('Add  foo').then(result => {
+        expect(result.valid).toBe(false);
+        expect(result.errors[0].name).toEqual('function-rules/type-enum');
+      });
+    });
+
+    it('should return an error if the commit has non-alphanumeric characters', () => {
+      return lint('Add @foo').then(result => {
+        expect(result.valid).toBe(false);
+        expect(result.errors[0].name).toEqual('function-rules/type-enum');
+      });
+    });
+  });
+});
