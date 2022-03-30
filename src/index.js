0542f6b4b470aaa8f01d2d0f0456f4299df66606
Rafael Azevedo <rafael.nqda@gmail.com>
2022-03-30 10:44:39 +0100

    Update header rules

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 
 /**
+ * Regexes.
+ */
+
+const regexes = {
+  typeEnum: {
+    atleastTwoWords: /(\w.+\s).+/,
+    base: /^(Add|Bump|Fix|Improve|Release|Remove|Update) \S+(?: \S+)*$/,
+    startWith: /^(Add|Bump|Fix|Improve|Release|Remove|Update) .+$/,
+    whitespace: /^\S+(?: \S+)*$/
+  }
+};
+
+/**
  * Export commitlint config.
  */
 
@@ -13,19 +26,30 @@ module.exports = {
     'body-case': [2, 'always', 'sentence-case'],
     'body-full-stop': [2, 'always', '.'],
     'function-rules/type-enum': [2, 'always', parsed => {
-      const regex = /^(Add|Bump|Fix|Improve|Release|Remove|Update) (\w+\s)*\w+$/;
+      if (!parsed.header.match(regexes.typeEnum.startWith)) {
+        return [false, 'The commit must start with: Add|Bump|Fix|Improve|Release|Remove|Update'];
+      }
 
-      if (parsed.header.match(regex)) {
+      if (!parsed.header.match(regexes.typeEnum.atleastTwoWords)) {
+        return [false, 'The commit must have at least two words'];
+      }
+
+      if (!parsed.header.match(regexes.typeEnum.whitespace)) {
+        return [false, 'The commit must have a single whitespace between words'];
+      }
+
+      if (parsed.header.match(regexes.typeEnum.base)) {
         return [true];
       }
 
-      return [false, 'The commit must start with: (Add|Bump|Fix|Improve|Release|Remove|Update)'];
+      return [false, `The commit not match: ${regexes.typeEnum.base}`];
     }],
+    'header-full-stop': [2, 'never'],
     'header-max-length': [2, 'always', 52],
-    'scope-empty': [2, 'always'],
+    'scope-empty': [0],
     'subject-empty': [0],
     'trailer-exists': [0],
-    'type-case': [2, 'always', 'sentence-case'],
+    'type-case': [0],
     'type-empty': [0],
     'type-enum': [0]
   }
